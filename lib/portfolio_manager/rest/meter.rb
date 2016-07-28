613ac1a04b186d5e0423dc2c8bfe58edf6142311
Brian Ryckbost <bryckbost@gmail.com>
2016-07-28 14:06:55 -0400

    Add documentation for Meter#metrics

--- lib/portfolio_manager/rest/meter.rb
+++ lib/portfolio_manager/rest/meter.rb
@@ -27,6 +27,17 @@ module PortfolioManager
         perform_get_request("/property/#{property_id}/meter/list")
       end
 
+      ##
+      # This web service returns a list of metric values for a specific property
+      # and period ending date based on the specified set of metrics and
+      # measurement system. The property must already be shared with you. A full
+      # list of the reporting metrics that are available through this service
+      # and related web services can be found here. The list includes the metric
+      # name, the appropriate web service call for the metric, and a glossary
+      # link. It can be sorted and filtered for ease of finding the metrics that
+      # you need.
+      #
+      # @see https://portfoliomanager.energystar.gov/webservices/home/api/reporting/propertyMetrics/get
       def metrics(property_id, year, month, measurement_system, metric)
         perform_get_request(
           "/property/#{property_id}/metrics",
@@ -38,6 +49,8 @@ module PortfolioManager
           }
         )
       end
+
+
     end
   end
 end
